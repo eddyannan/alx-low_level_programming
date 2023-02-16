a6240b1b42c592804a040f9bc6e2ce0b9a660a98
Eddy Annan <83912775+eddyannan@users.noreply.github.com>
2023-02-16 22:11:33 +0000

    Assembler a script that generates the assembly code of a C code and save it in an output file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * The main function is the entry point of the program.
+ * It prints a message to the console and returns 0 to indicate success.
+ */
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
+}
