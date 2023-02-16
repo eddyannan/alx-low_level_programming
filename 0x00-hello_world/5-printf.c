a40990ea99ea0e59816ef1258b6e7016723f6030
Eddy Annan <83912775+eddyannan@users.noreply.github.com>
2023-02-16 23:36:28 +0000

    printf message

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - function is the entry point of the program.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
