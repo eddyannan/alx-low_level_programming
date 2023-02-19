d8181db3810e6d6ae0218795de30343ae197e334
Eddy Annan <83912775+eddyannan@users.noreply.github.com>
2023-02-19 21:17:28 +0000

    updated file that prints exact statement

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,8 @@
  */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char *message =
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	const int length = 59; /* length of the message*/
 
 	/*Write the message to the standard error*/
