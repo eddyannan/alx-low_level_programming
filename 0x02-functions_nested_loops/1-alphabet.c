c4b61b184f7835bdf828b816fa40e431fdbc1ddf
Eddy Annan <83912775+eddyannan@users.noreply.github.com>
2023-02-21 21:31:04 +0000

    print lowcase of aplhabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ *return: void
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,8 @@
-#include <unistd.h>
+/* main.h */
+#ifndef MAIN_H
+#define MAIN_H
+/* functon prototypes*/
+int _putchar(char c);
+void print_alphabet(void);
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+#endif /* MAIN_H */
