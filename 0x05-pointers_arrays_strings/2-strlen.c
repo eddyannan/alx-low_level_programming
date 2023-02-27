554daa0a62053754c59814ea01a79aebbadb2ae9
Eddy Annan <83912775+eddyannan@users.noreply.github.com>
2023-02-27 19:44:33 +0000

    code computes lenght of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+/**
+ * _strlen - compute lenght of string
+ * @s: put string together
+ * Return: lenght
+ */
+#include <stdio.h>
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
+}
