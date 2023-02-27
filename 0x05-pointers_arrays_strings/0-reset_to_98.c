50f328dcb969a8b836bbea5078a44c4912eef202
Eddy Annan <83912775+eddyannan@users.noreply.github.com>
2023-02-27 18:24:17 +0000

    update value of n to 98 using pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * reset_to_98 - resets value to 98
+ * @n: The value to update
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+/* main.h */
+#ifndef MAIN_H
+#define MAIN_H
+/* functon prototypes*/
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif /* MAIN_H */
