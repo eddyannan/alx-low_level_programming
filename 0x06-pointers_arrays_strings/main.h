1a4a81efbe23ee9dffd727885c9296aa3eceb744
Eddy Annan <83912775+eddyannan@users.noreply.github.com>
2023-03-02 21:43:42 +0000

    updated files with fuction that copies string and one that concatenates strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,5 +4,7 @@
 /* functon prototypes*/
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
+char *_strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
 
 #endif /* MAIN_H */
