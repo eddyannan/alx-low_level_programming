dcde643e34bb8652979942458fa48f4641c7890d
Eddy Annan <83912775+eddyannan@users.noreply.github.com>
2023-03-07 19:04:02 +0000

    a function that prints the chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * print_chessboard - a function that prints a chessboard
+ * @a: array pointer
+ *
+ *
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+	for (j = 0; j < 8; j++)
+		{
+		printf("%c ", a[i][j]);
+		}
+		printf("\n");
+	}	
+}
