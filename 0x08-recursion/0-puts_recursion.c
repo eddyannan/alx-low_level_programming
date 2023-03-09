aad4b15cb3c307c7168d53a9c0af03415ad8280c
Eddy Annan <83912775+eddyannan@users.noreply.github.com>
2023-03-09 00:01:47 +0000

    a function that prints a string, followed by a new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - check the code
+ * @s: string pointer
+ *
+ * Return: return void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')  /* Base case: end of string reached */
+	{
+		putchar('\n');
+		return;
+	}
+	putchar(*s);  /* Print the current character */
+	/* Recursively call the function with the next character */
+	_puts_recursion(s + 1);
+}
