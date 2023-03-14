34e77c18ef6834742b573d6472cb7070b0e99588
Eddy Annan <83912775+eddyannan@users.noreply.github.com>
2023-03-14 17:25:45 +0000

    This functio  concatenates all the arguments of your program

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * argstostr - concatenates all the arguments of program
+ * @av: pointer to pointer to character
+ * @ac: int ac
+ * Return: return string
+ */
+char *argstostr(int ac, char **av)
+{
+	int length;
+	int i;
+	char *str;
+	int index;
+
+	if (ac == 0 || av == NULL)
+	{
+	return (NULL);
+	}
+/* Calculate the total length of the concatenated string */
+	length = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+	/* Add 1 for the newline character */
+		length += strlen(av[i]) + 1;
+	}
+/* Allocate memory for the concatenated string */
+	str = (char *)malloc(length * sizeof(char));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+/* Concatenate the strings */
+	index = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		strcpy(&str[index], av[i]);
+		index += strlen(av[i]);
+		str[index++] = '\n';
+	}
+		str[index] = '\0';
+		return (str);
+}
